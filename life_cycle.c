38116f01baf3c1bd2af2a8e7014a207cd0de3f0d
mzomeno- <misaclumsy@gmail.com>
2021-11-04 14:08:35 +0100

    Actualized utils

--- life_cycle.c
+++ life_cycle.c
@@ -6,7 +6,7 @@
 /*   By: mzomeno- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 13:41:07 by mzomeno-          #+#    #+#             */
-/*   Updated: 2021/11/04 13:03:56 by mzomeno-         ###   ########.fr       */
+/*   Updated: 2021/11/04 14:02:12 by mzomeno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,22 +31,30 @@ static int	lock_fork(t_forks *forks, int fork_index)
 ** If the philo manages to take both forks, print a message
 ** else, free the right fork and return
 */
-void		take_forks(int philo_id, int last_philo, t_config *common)
+void	take_forks(int philo_id, int last_philo, t_config *common)
 {
-	int	left_fork;
-	int	right_fork;
+	int		left_fork;
+	int		right_fork;
+	bool	left_fork_taken;
+	bool	right_fork_taken;
 
 	left_fork = philo_id;
 	if (philo_id == last_philo)
 		right_fork = 0;
 	else
 		right_fork = philo_id + 1;
-	if (lock_fork(common->forks, left_fork) == FAIL)
-		return;
-	if (lock_fork(common->forks, right_fork) == FAIL)
-		pthread_mutex_unlock(forks[left_fork]->mutex);
-	else
-		printer(FORKS_TAKEN, philo_id, common);
+	left_fork_taken = false;
+	right_fork_taken = false;
+	while (left_fork_taken = false || right_fork_taken = false)
+	{
+		if (right_fork_taken == true)
+			pthread_mutex_unlock(forks[right_fork]->mutex);
+		else if (left_fork_taken == true)
+			pthread_mutex_unlock(forks[left_fork]->mutex);
+		left_fork_taken = (bool)lock_fork(common->forks, left_fork);
+		right_fork_taken = (bool)lock_fork(common->forks, right_fork);
+	}
+	printer(FORKS_TAKEN, philo_id, common);
 }
 
 void	eat()
--- utils.c
+++ utils.c
@@ -6,22 +6,37 @@
 /*   By: mzomeno- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 21:50:36 by mzomeno-          #+#    #+#             */
-/*   Updated: 2021/11/04 09:41:43 by mzomeno-         ###   ########.fr       */
+/*   Updated: 2021/11/04 14:08:18 by mzomeno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	printer(t_philosopher *philo, const char *s)
+void	printer(const char *message, int philo_id, t_config *common)
 {
 	struct timeval	time;
 
-	pthread_mutex_lock(philo->p->printer);
+	pthread_mutex_lock(common->printer);
 	gettimeofday(&time, NULL);
-	if (philo->p->on || !ft_strcmp(DIE, s))
-		printf("%li %i %s\n", get_timeval(time, philo->p->start),
-			philo->id, s);
-	pthread_mutex_unlock(philo->p->printer);
+	if (common->stop_simulation == false || !ft_strcmp(DIE, message))
+		printf("%li %i %s\n", get_time_lapse(time, common->start_time),
+			philo_id, s);
+	pthread_mutex_unlock(common->printer);
+}
+
+int				ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (1);
+		s1++;
+		s2++;
+	}
+	if (*s1 || *s2)
+		return (1);
+	else
+		return (0);
 }
 
 int				ft_strlen(char *str)
